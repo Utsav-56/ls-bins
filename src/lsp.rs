9680bfc119caf644f91a30b8b6cb86b1f3a8c62b
Utsav-56 <utsavpokhrel200@gmail.com>
2025-03-13 20:38:06 +0545

    Added lsp logic

--- src/lsp.rs
+++ src/lsp.rs
@@ -1,1 +1,17 @@
 
+use colored::*;
+
+pub fn run() {
+    if let Ok(path) = std::env::var("PATH") {
+        println!("{}", "Current Shell PATH Variables:".bold().underline());
+        for (index, path) in path.split(':').enumerate() {
+            println!("{}: {}", index, path.green());
+        }
+    } else {
+        eprintln!("{}", "Error: Unable to retrieve PATH variable".red());
+    }
+}
+
+fn main() {
+    run();
+}
